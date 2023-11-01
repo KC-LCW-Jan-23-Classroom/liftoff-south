664758bf597bfd0496d47172128a5f086a0a2bb5
yep11 <129946792+yep11@users.noreply.github.com>
2023-11-01 06:12:05 -0500

    search forms by filter created

--- Booze-Blender/src/main/java/boozeblender/models/Search.java
+++ Booze-Blender/src/main/java/boozeblender/models/Search.java
@@ -1,4 +1,16 @@
 package boozeblender.models;
 
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
 public class Search {
+
+
+    @NotBlank(message = "Please enter a valid search parameter. It cannot be blank.")
+    @Size(min = 5, max = 20)
+    @NotNull(message = "Please enter a valid search parameter.")
+    private String searchParameter;
 }
