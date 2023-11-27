67c979452aeff0c3f6a1f541b0d82d0ed051f8d4
Kasandra Graciano <129809231+kgraciano@users.noreply.github.com>
2023-11-26 21:11:25 -0600

    search styling (#95)
    * search by glass styling and link to results
    
    * display glass results

--- Booze-Blender/src/main/java/boozeblender/controllers/SearchController.java
+++ Booze-Blender/src/main/java/boozeblender/controllers/SearchController.java
@@ -5,10 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URI;
@@ -75,10 +72,6 @@ public class SearchController {
         return "search/byIngredientResults";
     }
 
-//    @PostMapping("/byIngredient")
-//    public String searchByIngredient (@ModelAttribute Search search, Errors errors, Model model) {
-//        return "search/byIngredientResults";
-//    }
 
     @GetMapping("/byGlass")
     public String searchByGlass(Model model) {
@@ -86,48 +79,34 @@ public class SearchController {
         return "search/byGlass";
     }
 
-    @PostMapping("/byGlassResults")
-    public String searchByGlass(@ModelAttribute Search search, Errors errors, Model model) throws IOException, InterruptedException {
-
-        if (errors.hasErrors()) {
-            System.out.println(errors.getAllErrors());
-
-            return "search/byGlass";
-        }
-
+    @GetMapping("/byGlassResults")
+    public String searchByGlass(@RequestParam String glassType, Model model) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
 
-
-        String searchParameter = search.getSearchParameter();
-        String encodedSearchParameter = URLEncoder.encode(searchParameter, StandardCharsets.UTF_8);
+        String encodedSearchParameter = URLEncoder.encode(glassType, StandardCharsets.UTF_8);
         String url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=" + encodedSearchParameter;
+        System.out.println("Request URL: " + url);
+        System.out.println("Search Parameters: " + glassType);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .build();
 
         HttpResponse<String> response =
                 client.send(request, HttpResponse.BodyHandlers.ofString());
-
         String responseString = response.body().toString();
-        if (responseString.length() == 0) {
-            errors.rejectValue("searchParameter", "", "Invalid input. Please try again.");
-
-            return "search/byGlass";
-        }
 
         ObjectMapper mapper = new ObjectMapper();
         Map<String,Object> map = mapper.readValue(response.body(), Map.class);
 
         ArrayList drinksArray = (ArrayList) map.get("drinks");
 
+        model.addAttribute("glassType", glassType);
         model.addAttribute("drinksArray", drinksArray);
-        System.out.println(response.body());
-        model.addAttribute("response", response.body().toString());
-        model.addAttribute("search", search.getSearchParameter());
 
         return "search/byGlassResults";
-
     }
+
+
 //www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic
 //www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic
     @GetMapping("/alcoholicFilter")
@@ -203,9 +182,38 @@ public class SearchController {
         return "search/randomGeneratorResults";
     }
 
-//    @PostMapping("/randomCocktailGenerator")
-//    public String randomCocktailGenerator (@ModelAttribute Search search, Errors errors, Model model) {
-//        return "index";
-//    }
+    @GetMapping("/recipe")
+    public String displayRecipe(@RequestParam String cocktailId, Model model) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        String encodedId = URLEncoder.encode(cocktailId, StandardCharsets.UTF_8);
+        String url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + encodedId;
+        System.out.println("Request URL: " + url);
+        System.out.println("ID: " + encodedId);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+        String responseString = response.body().toString();
+        // Map JSON string to an object
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = mapper.readValue(response.body(), Map.class);
+
+        // the API returns an object containing an array with a key of "drinks".
+        // So get the value for "drinks", and cast it to an ArrayList so that we can iterate over the drinks.
+        ArrayList drinksArray = (ArrayList) map.get("drinks");
+
+
+        model.addAttribute("drinksArray", drinksArray);
+        model.addAttribute("cocktailId", cocktailId);
+
+
+        // Pass a list of drinks to the template
+        System.out.println(response.body());
+        return "search/recipe";
+    }
 
 }
